a040f63029276971fcbf2b1c8f2a36aa03e642b9
Lucas <lucascomaru20@gmail.com>
2020-12-20 03:06:27 -0300

    Ex 019, 020 Resolvidos

---
+++ ex020.py
@@ -0,0 +1,11 @@
+"Faça um programa que leia o nome dos alunos e mostre a ordem sorteada."
+
+import random
+n1 = str(input('Primeiro aluno :'))
+n2 = str(input('Segundo aluno :'))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno : '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem da aprensentação será :')
+print(lista)
