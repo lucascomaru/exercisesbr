81b4e6f394e6cc4d349ae3f7b8ab7b96d39a6123
Lucas <lucascomaru20@gmail.com>
2021-01-28 02:54:16 -0300

    Novo ex adicionado em resolução

--- ex045.py
+++ ex045.py
@@ -1,2 +1,23 @@
 """Crie um programa que faça o computador jogar Jokenpô com você."""
 
+from random import randint
+itens = ('Pedra', 'Papel', 'Tesoura')
+comp = randint(0, 2)
+print('''Suas opções:
+[ 1 ] PEDRA
+[ 2 ] PAPEL
+[ 3 ] TESOURA''')
+jogador = int(input('Qual é a sua jogada?'))
+print(f'Computador jogou {itens[comp]}' )
+print(f'Jogador jogou {itens[comp]}')
+if comp == jogador:
+    print('Empate')
+elif jogador == 1:
+    print('JOGADOR VENCEU! ')
+elif jogador == 2:
+    print('COMPUTADOR VENCEU! ')
+else:
+    print('JOGADA INVÁLIDA')
+
+
+
