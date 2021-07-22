59df70e08298b22070006602b52097e039081823
Lucas <lucascomaru20@gmail.com>
2021-07-22 20:56:36 -0300

    Novo ex adicioonado

--- ex088.py
+++ ex088.py
@@ -12,3 +12,13 @@ while True:
     resp = str(input('Quer continuar? [S/N] '))
     if resp in 'Nn':
         break
+print(f'{"No.":<4}{"Nome:<10"}{"Média":>8}')
+for i, a in enumerate(ficha):
+    print(f'{1:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe) '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc <= len(ficha) -1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
